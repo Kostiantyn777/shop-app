9a28abd4d8e1e34183d153a9df2d41c3189867c9
Kos <locoprodukten@gmail.com>
2020-07-28 20:41:23 +0200

    Items Creation and Prisma Yoga Flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,10 +1,27 @@
+//In my resolvers: Query.js and Mutation.js, I write all the logic that needs to happen
+
+
 const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    //TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+
+    return item;
+  },
   //createDog(parent, args, ctx, info) {
-    //global.dogs = global.dogs || [];
-    //create a dog 
-    //const newDog = {name: args.name};
-    //global.dogs.push(newDog);
-    //return newDog;
+  //global.dogs = global.dogs || [];
+  //create a dog
+  //const newDog = {name: args.name};
+  //global.dogs.push(newDog);
+  //return newDog;
   //},
 };
 
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,13 @@
+//In my resolvers: Query.js and Mutation.js, I write all the logic that needs to happen
+
+const { forwardTo } = require("prisma-binding");
+
 const Query = {
-  //dogs(parent, args, ctx, info) {
-    //global.dogs = global.dogs || [];
-    //return global.dogs;
+  // I CAN USE forwardTo  when I  do not need custom logic or aunthentification between Graphql Yoga server and my Prisma database
+  items: forwardTo("db"),
+  //async items(parent, args, ctx, info) {
+  //const items = await ctx.db.query.items();
+  //return items;
   //},
 };
 
