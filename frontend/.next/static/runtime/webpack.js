f34cc01c423b14c1415b47c36aa5b3d9779a5287
Kos <locoprodukten@gmail.com>
2020-08-11 20:43:03 +0200

    Deleting Items Part1-From backend

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0a9c17569293b9a34c3b";
+/******/ 	var hotCurrentHash = "38c1ee29d1509c978079";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
