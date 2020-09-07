37479d66161ce7e45cf0c4dc23d1ad851ef840db
Kos <locoprodukten@gmail.com>
2020-09-07 20:58:09 +0200

    Creating Shopping  Cart in React

--- frontend/.next/static/development/pages/index.js
+++ frontend/.next/static/development/pages/index.js
@@ -29845,7 +29845,7 @@ var Home = function Home(props) {
 
 /***/ }),
 
-/***/ 3:
+/***/ 7:
 /*!******************************!*\
   !*** multi ./pages/index.js ***!
   \******************************/
@@ -29870,5 +29870,5 @@ module.exports = dll_c47e886f146f98ef8f7b;
 
 /***/ })
 
-},[[3,"static/runtime/webpack.js"]]]));;
+},[[7,"static/runtime/webpack.js"]]]));;
 //# sourceMappingURL=index.js.map
--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8aeab2d60525b3bcab22";
+/******/ 	var hotCurrentHash = "3b30b06a2ecdf7b7f513";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
