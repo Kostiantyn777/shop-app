8101e241a99a3a4be03cee782ef56c34a508a16e
Kos <locoprodukten@gmail.com>
2020-09-13 17:13:05 +0200

    Animating Cart Count Component

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d4ede1b3fefaba6f2e21";
+/******/ 	var hotCurrentHash = "86da7b940dc6ac73e6d5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
